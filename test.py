7252f887c1d5039bacaa544cd5516cd89b495882
chaimabelgaied <58211463+chaimabelgaied@users.noreply.github.com>
2019-12-18 13:55:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ nom_fichier = sys.argv[1]
 test_image = image.load_img('data/prediction/voiture_ou_avion.jpg', target_size = (150, 150))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
+result = model.predict(test_image = test_image/255)
 if result[0][0] == 1:
     prediction = 'avion'
 else:
